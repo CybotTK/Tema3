26d8add6ec9f4551628b1538a6f0332c1775dca0
Cybot <stancilaionutmarian@yahoo.com>
2023-05-23 15:11:23 +0300

    aranjare mai frumoasa a codului

--- Abonat_Skype.h
+++ Abonat_Skype.h
@@ -8,11 +8,12 @@ class Abonat_Skype: public Abonat{
 
     public:
         Abonat_Skype();
+        ~Abonat_Skype();
+        
         Abonat_Skype(const std::string ID_skype, const std::string Nr_telefon, const int ID, const std::string Nume);
         Abonat_Skype(const std::string ID_skype, const Abonat& a);
         Abonat_Skype(const Abonat_Skype& other);
 
-        virtual ~Abonat_Skype();
         void Afisare() override;
         std::string GetNume() override;
         Abonat_Skype& operator= (const Abonat_Skype &other);
